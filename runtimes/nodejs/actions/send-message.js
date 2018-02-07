24fd5fd4c414b6030bc03e98e1e8b6573a04c03c
Belinda Vennam <beemarie88@gmail.com>
2018-02-07 16:37:18 -0600

    remove unused libraries

--- runtimes/nodejs/actions/send-message.js
+++ runtimes/nodejs/actions/send-message.js
@@ -1,6 +1,3 @@
-var request = require('request');
-var request_promise = require('request-promise');
-
 /*
   Takes information from a github event and puts it in a form consumable by
   the whisk.system/slack/post action.
