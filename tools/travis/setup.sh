194af59b7541f6a49c319e8cc2a91d1c5590a9da
Belinda Vennam <beemarie@us.ibm.com>
2018-03-07 14:29:05 -0600

    install alarms

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -3,6 +3,7 @@
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 HOMEDIR="$SCRIPTDIR/../../../"
 DEPLOYDIR="$HOMEDIR/openwhisk/catalog/extra-packages/packageDeploy"
+ALARMSDIR="$HOMEDIR/openwhisk/catalog/extra-packages/alarms-package"
 
 # jshint support
 sudo apt-get -y install nodejs npm
@@ -17,6 +18,9 @@ git clone --depth 1 https://github.com/apache/incubator-openwhisk.git openwhisk
 
 # shallow clone deploy package repo.
 git clone --depth 1 https://github.com/apache/incubator-openwhisk-package-deploy $DEPLOYDIR
+
+# shallow clone alarms package repo.
+git clone --depth 1 https://github.com/apache/incubator-openwhisk-package-alarms $ALARMSDIR
 
 cd openwhisk
 ./tools/travis/setup.sh
